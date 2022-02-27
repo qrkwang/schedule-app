0930cecca8c5ccc572ce017affea9e172fefdc55
Qi Rong Kwang <59965128+qrkwang@users.noreply.github.com>
2022-02-27 21:37:16 +0800

    calendar added

--- src/App.js
+++ src/App.js
@@ -1,31 +1,69 @@
 import React from "react";
-import {Grid, Button, Box, Paper} from "@mui/material";
 import { styled } from '@mui/material/styles';
+import Box from '@mui/material/Box';
+import Paper from '@mui/material/Paper';
+import Grid from '@mui/material/Grid';
+import { Calendar, momentLocalizer } from 'react-big-calendar'
+import moment from 'moment'
+import events from './events'
+import "react-big-calendar/lib/css/react-big-calendar.css";
+
+const localizer = momentLocalizer(moment)
+const MyCalendar = props => (
+    <div>
+        <Calendar
+            localizer={localizer}
+            events={events}
+            startAccessor="start"
+            endAccessor="end"
+            style={{ height: 1000 }}
+        />
+    </div>
+)
 
 const Item = styled(Paper)(({ theme }) => ({
+    backgroundColor: theme.palette.mode === 'dark' ? '#1A2027' : '#fff',
     ...theme.typography.body2,
     padding: theme.spacing(1),
     textAlign: 'center',
     color: theme.palette.text.secondary,
 }));
 
+
+
 const App = () => {
     return (
-        <Grid container spacing={2}>
-            <Grid item xs={8}>
-                <Item>xs=8</Item>
-
+        <Box sx={{ flexGrow: 1 }}>
+            <Grid container spacing={2}>
+                <Grid item xs={8}>
+                    <Item>xs=8</Item>
+                </Grid>
+                <Grid item xs={4}>
+                    <Item>xs=4</Item>
+                </Grid>
+                <Grid item xs={4}>
+                    <Item>xs=4</Item>
+                </Grid>
+                <Grid item xs={8}>
+                    <MyCalendar></MyCalendar>
+                </Grid>
             </Grid>
-            <Grid item xs={4}>
-                <Item>xs=4</Item>
-            </Grid>
-            <Grid item xs={4}>
-                <Item>xs=4</Item>
-            </Grid>
-            <Grid item xs={8}>
-                <Item>xs=8</Item>
-            </Grid>
-        </Grid>
+        </Box>
+        // <Grid container spacing={2}>
+        //     <Grid item xs={8}>
+        //         <Item>xs=8</Item>
+        //
+        //     </Grid>
+        //     <Grid item xs={4}>
+        //         <Item>xs=4</Item>
+        //     </Grid>
+        //     <Grid item xs={4}>
+        //         <Item>xs=4</Item>
+        //     </Grid>
+        //     <Grid item xs={8}>
+        //         <Item>xs=8</Item>
+        //     </Grid>
+        // </Grid>
     )
 }
 
